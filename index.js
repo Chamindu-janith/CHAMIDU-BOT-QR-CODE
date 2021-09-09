fcce2424126c0c064739c87a81d99a81f5022c42
chamidu janith <78243895+Chamindu-janith@users.noreply.github.com>
2021-09-09 06:34:40 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const chalk = require('chalk');
+const { WAConnection, MessageType } = require('@adiwajshing/baileys');
+const fs = require('fs');
+
+async function CHAMIBOT() {
+	const conn = new WAConnection();
+	conn.logger.level = 'warn';
+	conn.version = [2, 2119, 6]
+
+	conn.on('connecting', async () => {
+		console.log(`${chalk.green.bold('T')}${chalk.blue.bold('CHAMI')}
+${chalk.white.italic('CHAMIBOT String SESSION')}
+${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please wait.')}`);
+	});
+
+	conn.on('open', async () => {
+		console.log(
+			chalk.green.bold('CHAMI-BOT QR Code: '),
+			'CHAMIBOT;;;' +
+				Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
+					'base64'
+				)
+		);
+		await conn.sendMessage(
+			conn.user.jid,
+			'CHAMIBOT;;;' +
+				Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
+					'base64'
+				),
+			MessageType.text
+		);
+		if (conn.user.jid.startsWith('90')) {
+			await conn.sendMessage(
+				conn.user.jid,
+				'*⚠️ PLEASE DO NOT SHARE THIS CODE WITH ANYONE , කරුණාකර මෙම කේතය වෙන කිසිවෙකුට ලබා නොදෙන්න.. ' + conn.user.name + '* ⚠️',
+				MessageType.text
+			);
+		} else {
+			await conn.sendMessage(
+				conn.user.jid,
+				'*⚠️ Please Do Not Share This Code With Anyone , කරුණාකර මෙම කේතය වෙන කිසිවෙකුට ලබා නොදෙන්න...' +
+					conn.user.name +
+					'* ⚠️',
+				MessageType.text
+			);
+		}
+		console.log(
+			chalk.green.bold(
+				"IF YOU CANNOT COPY THE MESSAGEE , PLEASE CHECK WHATSAPP. QR CODE SENT TO YOUR OWN NUMBER!මෙම කේතය මෙතැන කොපි කල නොහැක කරුණාකර ඔබගේ වට්සැප් අන්කය පරීක්ශා කරන්න...\n"
+			),
+			chalk.green.bold(
+				'IF YOU CANNOT COPY THE MESSAGE, PLEASE CHECK WHATSAPP. QR CODE SENT TO YOUR OWN NUMBER!මෙම කේතය මෙතැන කොපි කල නොහැක කරුණාකර ඔබගේ වට්සැප් අන්කය පරීක්ශා කරන්න...'
+			)
+		);
+		process.exit(0);
+	});
+
+	await conn.connect();
+}
+
+CHAMIBOT();
